99f5a4613ff3050498347841b2559d3a2bb38336
sid-FBLA <sid20102004@gmail.com>
2022-03-07 18:55:08 -0500

    fixed up

--- script.js
+++ script.js
@@ -25,26 +25,97 @@ document.addEventListener("DOMContentLoaded", () => {
             menuButtonClose.classList.add("hidden");
         }
     });
-    //modifying Datepicker
-    const dateSubmissions = document.querySelectorAll(".submitDate");
-    for (i = 0; i < dateSubmissions.length; i += 1) {
-      let submitChosen = dateSubmissions[i];
-      dateSubmissions[i].addEventListener("click", function(e) {
-        const today = new Date();
-        console.log(today);
-        console.log(submitChosen);
-        const form = submitChosen.parentNode.parentNode;
-        console.log(form);
-        let selectedDate = form.querySelector(".datepicker").value;
-        console.log(selectedDate);
-        if(selectedDate < today) {
-          console.log("selectedDate is in the past");
-        }
-      })
+
+    function createInvalid(element, firstLine, secondLine) {
+      console.log("running");
+      const invalidText = document.createElement("P");
+      invalidText.classList.add("mt-2", "text-sm", "text-red-600", "dark:text-red-500");
+      element.appendChild(invalidText);
+      const invalidTextSpan = document.createElement("SPAN");
+      invalidTextSpan.classList.add("font-medium");
+      invalidTextSpan.innerHTML = firstLine;
+      invalidText.appendChild(invalidTextSpan);
+      invalidText.innerHTML = secondLine;
     }
+    //modifying Datepicker
+    const datePickers = document.querySelectorAll(".datepicker-input");
 
-    console.log(dateSubmissions);
+    for (i = 0; i < datePickers.length; i += 1) {
+      let datePicker = datePickers[i];
+      console.log(datePicker);
+      datePicker.addEventListener('click', function(e) {
+        let dateOptions = document.querySelectorAll(".datepicker-picker .datepicker-main .datepicker-view .days .datepicker-grid span");
+        let changeMonth = document.querySelectorAll(".datepicker-picker .datepicker-controls button");
+        //Reset date options if month is changed -- may be unecessary
+        for (i = 0; i < changeMonth.length; i += 1) {
+          changeMonth[i].addEventListener('click', function() {
+            dateOptions = document.querySelectorAll(".datepicker-picker .datepicker-main .datepicker-view .days .datepicker-grid span");
+          });
+        }
+        for (i = 0; i < dateOptions.length; i += 1) {
+          //after a date has been selected
+          dateOptions[i].addEventListener('click', function() {
+            let today = new Date();
+            tomorrow = '0'+(today.getMonth()+1)+'/'+'0'+(today.getDate()+1)+'/'+today.getFullYear();
+            today = '0'+(today.getMonth()+1)+'/'+'0'+today.getDate()+'/'+today.getFullYear();
+            console.log(today);
+            let selectedDate = datePicker.value;
+            //delay needed to retrieve value
+            setTimeout(() => {
+              selectedDate = datePicker.value; console.log(selectedDate)
+              if (selectedDate <= today) {
 
+                selectedDate = tomorrow;
+                console.log(selectedDate);
+                console.log(datePicker);
+                datePicker.value = tomorrow;
+                const form = datePicker.parentNode.parentNode;
+                console.log(form);
+                datePicker.classList.add("border-red-600");
+                const invalidText = form.querySelector("p");
+                invalidText.classList.remove("hidden");
+                invalidText.classList.add("block");
+
+            }
+          }, 500);
+        });
+      };
+    });
+  };
+  const sessionForms = document.querySelectorAll("form");
+  const submitAlert = document.querySelector(".submitAlert");
+  const initText = submitAlert.querySelector("p").innerHTML;
+  submitAlert.style.height = "0";
+  $(".submitAlert").css('overflow', 'hidden');
+  for (let i = 0; i < sessionForms.length; i += 1) {
+    console.log(sessionForms[i]);
+    sessionForms[i].addEventListener('submit', function(e) {
+      e.preventDefault();
+      console.log(e);
+      if (sessionForms[i].querySelector('button').innerHTML === "Subscribe") {
+          console.log(sessionForms[i]);
+          submitAlert.querySelector("p").innerHTML = "Thank you for subscribing!";
+      }
+      if (submitAlert.classList.contains("hidden")) {
+        console.log("first part working");
+        console.log($(".submitAlert"));
+        submitAlert.classList.add("block");
+        submitAlert.classList.remove("hidden");
+        $(".submitAlert").height(0).animate({height: 52}, 250);
+        const close = submitAlert.querySelector("button");
+        close.addEventListener('click', function() {
+          $(".submitAlert").height(52).animate({height: 0}, 250);
+          setTimeout(() => {
+            console.log(submitAlert.offsetHeight);
+            submitAlert.offsetHeight = 0;
+            submitAlert.classList.add("hidden")
+            submitAlert.classList.remove("block");
+            submitAlert.querySelector("p").innerHTML = initText;
+          }, 250);
+        })
+      }
+    });
+  }
 
 
 });
